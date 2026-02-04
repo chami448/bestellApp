5f6cfc382bfd59962430168bce872416b4af689a
chami448 <155586123+chami448@users.noreply.github.com>
2026-02-04 17:15:21 +0100

    update db

---
+++ scripts/db.js
@@ -0,0 +1,94 @@
+let myDishes = [
+    {
+        "name" : "Fufu",
+        "price": 5,
+        "description": "A traditional West African dish made from cassava flour, often served with soup or stew.",
+    },
+    {
+        "name" : "Jollof Rice",
+        "price": 7,
+        "description": "A popular West African dish made with rice, tomatoes, onions, and various spices.",
+    },
+    {
+        "name" : "Pepper Stew",
+        "price": 6,
+        "description": "A spicy stew made with peppers, tomatoes, onions.",
+    },
+    {
+        "name": "Casava Leaves",
+        "price": 8,
+        "description": "A dish made from the leaves of the cassava plant, often cooked with palm oil and spices.",
+    },
+    {
+        "name": "Okra Soup",
+        "price": 9,
+        "description": "A flavorful soup made with okra, tomatoes, onions, and various spices.",
+    },
+    {
+        "name": "Capenta",
+        "price": 10,
+        "description": "A dish made from small dried fish, often served with sadza or rice.",
+    },
+    {
+        "name": "Ogbono Soup",
+        "price": 11,
+        "description": "A thick soup made from ground ogbono seeds, often cooked with meat and vegetables.",
+    },
+    {
+        "name": "Madesu",
+        "price": 12,
+        "description": "A dish made from black-eyed peas, often cooked with tomatoes and spices.",
+    }
+
+]
+
+let myDrinks = [
+    {
+        "name": "Coca-Cola",
+        "price": 3.50,
+    },
+    {
+        "name": "Fanta",
+        "price": 3.50,
+    },
+    {
+        "name": "Sprite",
+        "price": 3.50,
+    },
+    {
+        "name": "Water",
+        "price": 2.00,
+    },
+    {
+        "name": "Orange Juice",
+        "price": 4.00,
+    },
+    {
+        "name": "Apple Juice",
+        "price": 4.00,
+    },
+    {
+        "name": "Mango Juice",
+        "price": 4.00,
+    }
+]
+
+
+let myalcoholicDrinks = [
+    {
+        "name": "Krombacher Pils",
+        "price": 5.00,
+    },
+    {
+        "name": "Heineken",
+        "price": 5.00,
+    },
+    {
+        "name": "Guinness",
+        "price": 6.00,
+    },
+    {
+        "name": "Red Wine",
+        "price": 7.00,
+    }
+]
