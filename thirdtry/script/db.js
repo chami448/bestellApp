11f0d90d04931458577f15bc77b46f28e5f4adba
chami448 <155586123+chami448@users.noreply.github.com>
2026-04-05 21:22:52 +0200

    add db and template

---
+++ thirdtry/script/db.js
@@ -0,0 +1,163 @@
+let myDishes = [
+  {
+    name: "Fufu - 300g",
+    price: 5.0,
+    description:
+      "A traditional West African dish made from cassava flour, often served with soup or stew.",
+  },
+  {
+    name: "Jollof Rice - 300g",
+    price: 7.0,
+    description:
+      "A popular West African dish made with rice, tomatoes, onions, and various spices.",
+  },
+];
+
+let mySupplements = [
+  {
+    name: "Pepper Stew",
+    price: 14.0,
+    description: "A spicy stew made with peppers, tomatoes, onions.",
+  },
+  {
+    name: "Casava Leaves",
+    price: 8.5,
+    description:
+      "A dish made from the leaves of the cassava plant, often cooked with palm oil and spices.",
+  },
+  {
+    name: "Okra Soup",
+    price: 12.5,
+    description:
+      "A flavorful soup made with okra, tomatoes, onions, and various spices.",
+  },
+  {
+    name: "Capenta",
+    price: 10.5,
+    description:
+      "A dish made from small dried fish, often served with sadza or rice.",
+  },
+  {
+    name: "Ogbono Soup",
+    price: 13.5,
+    description:
+      "A thick soup made from ground ogbono seeds, often cooked with meat and vegetables.",
+  },
+  {
+    name: "Madesu",
+    price: 12.0,
+    description:
+      "A dish made from black-eyed peas, often cooked with tomatoes and spices.",
+  },
+];
+
+let myDrinks = [
+  {
+    name: "Coca-Cola",
+    price: 3.5,
+    deposit: 0.25,
+  },
+  {
+    name: "Fanta",
+    price: 3.5,
+    deposit: 0.25,
+  },
+  {
+    name: "Sprite",
+    price: 3.5,
+    deposit: 0.25,
+  },
+  {
+    name: "Water",
+    price: 2.0,
+    deposit: 0.25,
+  },
+  {
+    name: "Orange Juice",
+    price: 4.0,
+    deposit: 0.25,
+  },
+  {
+    name: "Apple Juice",
+    price: 4.0,
+    deposit: 0.25,
+  },
+  {
+    name: "Mango Juice",
+    price: 4.0,
+    deposit: 0.25,
+  },
+];
+
+let myalcoholicDrinks = [
+  {
+    name: "Krombacher Pils",
+    price: 5.0,
+    deposit: 0.25,
+  },
+  {
+    name: "Heineken",
+    price: 5.0,
+    deposit: 0.25,
+  },
+  {
+    name: "Guinness",
+    price: 6.0,
+    deposit: 0.25,
+  },
+  {
+    name: "Red Wine",
+    price: 7.0,
+    deposit: 0.25,
+  },
+];
+
+let mealOfTheDay = {
+  name: "Jollof Rice with Okra Soup",
+  price: 15,
+  description:
+    "A delicious combination of spicy Jollof rice served with a hearty Okra soup.",
+};
+
+let favoriteDishes = [];
+
+const categoryMap = {
+  dishes: myDishes,
+  supplements: mySupplements,
+  alcoholicDrinks: myalcoholicDrinks,
+  drinks: myDrinks,
+  mealOfTheDay: [mealOfTheDay],
+  favorites: favoriteDishes,
+};
+
+let deliveryCost = 2.99;
+
+
+let tip = [
+  { name: "5%",
+    value: 0.05,
+  },
+  { name: "10%",
+    value: 0.1,
+  },
+  { name: "15%",
+    value: 0.15,
+  },
+]
+
+let basketItems = [];
+let deliveryMode = "delivery";
+
+const iconMap = {
+    addIcon: "./image/icon/add_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png",
+    likeIcon: "./image/icon/favorite_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png",
+    likedIcon: "./image/icon/heart_check_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png",
+    removeIcon: "./image/icon/remove_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png",
+  menuIcon: "./image/icon/menu_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png",
+    menuActiveIcon: "./image/icon/menu_open_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png",
+    trashIcon: "./image/icon/delete_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png",
+  logoImage: "./image/img/suggabearsclub-africa-9904843_640.png",
+  heroImage: "./image/img/buffetcrush-christmas-wallpaper-2009590_640.jpg",
+  bannerIcon: "./image/icon/clker-free-vector-images-place-setting-309980_640.png",
+  promoImage: "./image/img/luk1004-food-3083223_640.jpg",
+};
