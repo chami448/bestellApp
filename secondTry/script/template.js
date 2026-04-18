7559de9bb2635c22f9a367059cbc262b2c5dfbbe
chami448 <155586123+chami448@users.noreply.github.com>
2026-04-18 15:50:58 +0200

    add header & banner section

---
+++ secondTry/script/template.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+function renderHeader(){
+    return `
+            <header>
+                   <div class="headerContainer">
+                        <div class = "headLogo">
+                        <img src="${imageMap.headerLogo}" alt="Header Logo" class="headerLogo">
+                        </div>
+                        <div class = "headerTitle">
+                        <h1>Mama Sambia</h1>
+                        </div>
+                        <div class = "headBasketIcon">
+                        <img src="${iconMap.menuCloseIcon}" alt="Menu Icon" class="menuCloseIcon">
+                        </div>
+                   </div>
+            </header>
+    `;
+};
+
+
+function renderBannerHeader(){
+    return`
+    
+    
+    `;
+};
